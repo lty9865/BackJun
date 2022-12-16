97c136d515f3ae7f74654992a0a6b3599f2ff52c
Administrator <Administrator@User-2022VVQDAX>
2022-12-16 18:08:47 +0900

    commit

--- Backjun/src/q10828/Main.java
+++ Backjun/src/q10828/Main.java
@@ -1,52 +1,28 @@
 package q10828;
 
 import java.util.Scanner;
-import java.util.Stack;
 
 class Operation {
-	int count;
 	int result;
-	Stack<Integer> numO = new Stack<Integer>();
 
-	public Operation(int count) {
-		this.count = count;
+	public Operation() {
+
 	}
 
 	public Operation(String op, int num) {
+
 		switch (op) {
 		case "push":
-			numO.push(num);
 			break;
 		case "pop":
-			if (numO.empty()) {
-				System.out.println("-1");
-			} else {
-				result = numO.pop();
-				System.out.println(getResult());
-			}
 			break;
 		case "size":
-			System.out.println(numO.size());
 			break;
 		case "empty":
-			if (numO.empty()) {
-				System.out.println("1");
-			} else {
-				System.out.println("0");
-			}
 			break;
 		case "top":
-			if (numO.empty()) {
-				System.out.println("-1");
-			} else {
-				System.out.println(numO.peek());
-			}
 			break;
 		}
-	}
-
-	public int getCount() {
-		return count;
 	}
 
 	public int getResult() {
@@ -57,13 +33,13 @@ class Operation {
 
 public class Main {
 	public static void main(String[] args) {
-		Stack<Integer> num = new Stack<Integer>();
 		Scanner sc = new Scanner(System.in);
+		int length = sc.nextInt();
+		int[] mStack = new int[length];
 
-		int count = sc.nextInt();
-		for (int i = 0; i < count; i++) {
+		for (int i = 0; i < mStack.length; i++) {
 			String str = sc.nextLine();
-			if(str.isBlank()) {
+			if (str.isBlank()) {
 				str.split(str);
 			}
 
